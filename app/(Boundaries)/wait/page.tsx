2abd412971e4c1a72ee94d9361a82dece4bc8c92
shovian <hideyoshishofian@gmail.com>
2023-11-21 15:27:22 +0700

    debug: mobile deploy error

--- app/(Boundaries)/wait/page.tsx
+++ app/(Boundaries)/wait/page.tsx
@@ -8,15 +8,15 @@ const HalamanTunggu = () => {
   useEffect(() => {}, []);
   useEffect(() => {
     subscribeStatus(setStatus);
-    if (status === "Ready") {
-      var sound = new Howl({
-        src: ["/music/notif.mp3"],
-        volume: 0.5,
-        loop: true,
-      });
-      sound.play();
-      navigator.vibrate(200);
-    }
+    // if (status === "Ready") {
+    //   var sound = new Howl({
+    //     src: ["/music/notif.mp3"],
+    //     volume: 0.5,
+    //     loop: true,
+    //   });
+    //   sound.play();
+    //   navigator.vibrate(200);
+    // }
   }, [status]);
   return (
     <div className="h-screen w-screen bg-red-500 flex flex-col items-center justify-center">
