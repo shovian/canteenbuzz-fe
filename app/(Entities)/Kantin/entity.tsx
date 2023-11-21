4cea571f6e577824f4ecf6c791e28e4a8a749f0f
shovian <hideyoshishofian@gmail.com>
2023-11-21 15:07:04 +0700

    test changing repo

--- app/(Entities)/Kantin/entity.tsx
+++ app/(Entities)/Kantin/entity.tsx
@@ -14,7 +14,6 @@ import { Pembeli } from "../Pembeli/entity";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBCMkbts9TUfOOdlYJEH0Rxu-eZhFWRpJk",
