c701b0296b0d9f6350692e77ef352b457d47117f
shovian <hideyoshishofian@gmail.com>
2023-11-21 16:25:02 +0700

    bug resolved test

--- app/(Boundaries)/wait/page.tsx
+++ app/(Boundaries)/wait/page.tsx
@@ -4,19 +4,19 @@ import { useEffect, useState } from "react";
 import { Howl, Howler } from "howler";
 
 const HalamanTunggu = () => {
-  const [status, setStatus] = useState<String>("a");
+  const [status, setStatus] = useState<String>();
   useEffect(() => {}, []);
   useEffect(() => {
-    // subscribeStatus(setStatus);
-    // if (status === "Ready") {
-    //   var sound = new Howl({
-    //     src: ["/music/notif.mp3"],
-    //     volume: 0.5,
-    //     loop: true,
-    //   });
-    //   sound.play();
-    //   navigator.vibrate(200);
-    // }
+    subscribeStatus(setStatus);
+    if (status === "Ready") {
+      var sound = new Howl({
+        src: ["/music/notif.mp3"],
+        volume: 0.5,
+        loop: true,
+      });
+      sound.play();
+      navigator.vibrate(200);
+    }
   }, [status]);
   return status ? (
     <div className="h-screen w-screen bg-red-500 flex flex-col items-center justify-center">
